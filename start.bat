d44fb91a3aaf7f14539be0f1b27cf3833b4e03ce
longpros <40897449+longpros@users.noreply.github.com>
2018-07-07 00:18:31 +0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,7 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+timeout /t 20
+EthDcrMiner64.exe -epool eth-asia1.nanopool.org:9999 -ewal 0xdd89080e62398352aa57a04149aedf8ea7b49c26.580-2/hoangphilong.pro@gmail.com -epsw x -mode 1 -ftime 10 -cvddc 860 -mvddc 860 -cclock 1150
