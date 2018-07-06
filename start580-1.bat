6e802c0afe5b23b0f91500c959daa1890a0f0270
longpros <40897449+longpros@users.noreply.github.com>
2018-07-06 23:59:12 +0700

    Create start580-1.bat

---
+++ start580-1.bat
@@ -0,0 +1,9 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+timeout /t 20
+EthDcrMiner64.exe -epool eth-asia1.nanopool.org:9999 -ewal 0xa19449fea834e585b60e5828a1ed3c0ed9ddc41d.470-1/vienthongduchien@gmail.com -epsw x -mode 1 -ftime 10 -r 1
+
+ -cvddc 860 -mvddc 860 -powlim 15 -cclock 1150 -mclock 2200,2200,2200,2200,2200,2200
